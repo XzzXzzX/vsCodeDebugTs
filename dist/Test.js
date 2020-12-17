2262545d235ed6b588ba0e1e19b027f7373ec3fb
xuan <673194747@qq.com>
2020-12-17 14:42:12 +0800

    'update'

--- dist/Test.js
+++ dist/Test.js
@@ -89,7 +89,12 @@ function testSlice() {
 function sleep(second, param) {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
-            resolve(param);
+            if (param > 1000) {
+                resolve(param);
+            }
+            else {
+                reject(param);
+            }
         }, second);
     });
 }
@@ -100,7 +105,13 @@ function testAsync() {
             switch (_a.label) {
                 case 0:
                     console.log("testAsync1111", new Date().getTime());
-                    return [4 /*yield*/, sleep(1000)];
+                    return [4 /*yield*/, sleep(1000).then(function (param) {
+                            // resolve
+                            console.log('promise then resolve');
+                        }, function (param) {
+                            // reject
+                            console.log("promise then reject " + param);
+                        })];
                 case 1:
                     ret = _a.sent();
                     console.log('testAsync2222', ret);
@@ -126,7 +137,7 @@ function testHanzi() {
     var nameLen = 3;
     for (var index = 0; index < nameLen; index++) {
         // eval("var word=" + '\\u' + (Math.round(Math.random() * 20901) + 19968).toString(16) + '"');
-        var str = '\\u' + (Math.round(Math.random() * 20901) + 19968).toString(16);
+        var str_1 = '\\u' + (Math.round(Math.random() * 20901) + 19968).toString(16);
         // console.log("str: ", str);
         // let strs = str.split("\\u");
         // var str2 = '';
@@ -134,7 +145,7 @@ function testHanzi() {
         //     str2 += String.fromCharCode(Number(parseInt(strs[i], 16).toString(10)));
         //     console.log("str2: ", str2, i);
         // }
-        var str2 = unescape(str.replace(/\\u/gi, '%u'));
+        var str2 = unescape(str_1.replace(/\\u/gi, '%u'));
         name += str2;
     }
     name.trim();
@@ -178,4 +189,87 @@ function testObject() {
     console.log('url: ', QGRemoteUrl[sys.HUAWEI_GAME]);
 }
 testObject();
+function testPromise() {
+    var p = function () {
+        var num = Math.random();
+        return new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                num > 0.5 ? resolve(num) : reject(num);
+            }, 1000);
+        });
+    };
+    p().then(function (val) {
+        console.info("Status switches to fulfilled, and the value is " + val);
+    }, function (val) {
+        console.info("Status switches to reject, and the value is " + val);
+    });
+}
+testPromise();
+// testPromise().then(() => {
+//     console.log('====> then');
+// });
+function encodeUTF8(s) {
+    var i, r = [], c, x;
+    for (i = 0; i < s.length; i++)
+        if ((c = s.charCodeAt(i)) < 0x80)
+            r.push(c);
+        else if (c < 0x800)
+            r.push(0xC0 + (c >> 6 & 0x1F), 0x80 + (c & 0x3F));
+        else {
+            if ((x = c ^ 0xD800) >> 10 == 0) //对四字节UTF-16转换为Unicode
+                c = (x << 10) + (s.charCodeAt(++i) ^ 0xDC00) + 0x10000,
+                    r.push(0xF0 + (c >> 18 & 0x7), 0x80 + (c >> 12 & 0x3F));
+            else
+                r.push(0xE0 + (c >> 12 & 0xF));
+            r.push(0x80 + (c >> 6 & 0x3F), 0x80 + (c & 0x3F));
+        }
+    ;
+    return r;
+}
+// 字符串加密成 hex 字符串
+function sha1(s) {
+    var data = new Uint8Array(encodeUTF8(s));
+    var i, j, t;
+    var l = ((data.length + 8) >>> 6 << 4) + 16, s = new Uint8Array(l << 2);
+    s.set(new Uint8Array(data.buffer)), s = new Uint32Array(s.buffer);
+    for (t = new DataView(s.buffer), i = 0; i < l; i++)
+        s[i] = t.getUint32(i << 2);
+    s[data.length >> 2] |= 0x80 << (24 - (data.length & 3) * 8);
+    s[l - 1] = data.length << 3;
+    var w = [], f = [
+        function () { return m[1] & m[2] | ~m[1] & m[3]; },
+        function () { return m[1] ^ m[2] ^ m[3]; },
+        function () { return m[1] & m[2] | m[1] & m[3] | m[2] & m[3]; },
+        function () { return m[1] ^ m[2] ^ m[3]; }
+    ], rol = function (n, c) { return n << c | n >>> (32 - c); }, k = [1518500249, 1859775393, -1894007588, -899497514], m = [1732584193, -271733879, null, null, -1009589776];
+    m[2] = ~m[0], m[3] = ~m[1];
+    for (i = 0; i < s.length; i += 16) {
+        var o = m.slice(0);
+        for (j = 0; j < 80; j++)
+            w[j] = j < 16 ? s[i + j] : rol(w[j - 3] ^ w[j - 8] ^ w[j - 14] ^ w[j - 16], 1),
+                t = rol(m[0], 5) + f[j / 20 | 0]() + m[4] + w[j] + k[j / 20 | 0] | 0,
+                m[1] = rol(m[1], 30), m.pop(), m.unshift(t);
+        for (j = 0; j < 5; j++)
+            m[j] = m[j] + o[j] | 0;
+    }
+    ;
+    t = new DataView(new Uint32Array(m).buffer);
+    for (var i = 0; i < 5; i++)
+        m[i] = t.getUint32(i << 2);
+    var hex = Array.prototype.map.call(new Uint8Array(new Uint32Array(m).buffer), function (e) {
+        return (e < 16 ? "0" : "") + e.toString(16);
+    }).join("");
+    return hex;
+}
+var data = {
+    "nickName": "Band",
+    "gender": 1,
+    "language": "zh_CN",
+    "city": "Guangzhou",
+    "province": "Guangdong",
+    "country": "CN",
+    "avatarUrl": "http://wx.qlogo.cn/mmopen/vi_32/1vZvI39NWFQ9XM4LtQpFrQJ1xlgZxx3w7bQxKARol6503Iuswjjn6nIGBiaycAjAtpujxyzYsrztuuICqIM5ibXQ/0"
+};
+var str = JSON.stringify(data) + 'HyVFkGl5F5OQWJZZaNzBBg==';
+console.log('===> sha: ', sha1(str));
 //# sourceMappingURL=Test.js.map
